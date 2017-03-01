5a1d984b04db19d358a0f70c2cb5013c6a2a17b9
Vineeth Vattikuti <vinkriv@gmail.com>
2017-03-01 11:48:15 -0600

    v

--- app/src/main/java/com/example/a10017404/textmessageapp/MainActivity.java
+++ app/src/main/java/com/example/a10017404/textmessageapp/MainActivity.java
@@ -1,13 +1,43 @@
 package com.example.a10017404.textmessageapp;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.provider.Telephony;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.widget.TextView;
+
+import java.lang.reflect.Array;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    BroadcastReceiver receiver;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView)findViewById(R.id.textView);
+        receiver = new TextMonitor();
+        IntentFilter filter = new IntentFilter("android.provider.Telephony.SMS_RECEIVED");
+        registerReceiver(receiver,filter);
+    }
+
+    public class TextMonitor extends BroadcastReceiver{
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Bundle bundle = intent.getExtras();
+            Object[] pdus = (Object[])bundle.get("pdus");
+            SmsMessage[] messages = new SmsMessage[pdus.length];
+            for (int x=0;x<pdus.length;x++){
+                messages[x]=SmsMessage.createFromPdu((byte[])pdus[x],bundle.getString("format"));
+            }
+            textView.setText(messages[0].getMessageBody());
+        }
     }
 }
